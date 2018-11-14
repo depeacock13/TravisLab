ff28ed4f2cd63d81a18a75818670ae1c22b74563
depeacock13 <depeacock13@aol.com>
2018-11-14 11:54:35 -0800

    Add files via upload

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,18 @@
+/*  
+    David Peacock / SN8583
+    CS-401 Software Engineering
+    Due: Wed 14 Nov 2018 @ 1:30 PM
+    Lab 10: Travis Lab
+*/
+
+package helloworld;
+
+public class HelloWorld {
+
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+    
+}
